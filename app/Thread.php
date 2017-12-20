8760c7ef2e4ef343cafb4a2a7109192063bfa34d
LeeDark <sergey.povarnin@gmail.com>
2017-12-20 18:28:26 +0200

    21. Global Scopes and further Query reduction.

--- app/Thread.php
+++ app/Thread.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $guarded = [];
+    protected $with = ['creator', 'channel'];
 
     protected static function boot()
     {
         parent::boot();
+
+//        static::addGlobalScope('creator', function ($builder) {
+//            $builder->with('creator');
+//        });
 
         static::addGlobalScope('replyCount', function ($builder) {
            $builder->withCount('replies');
@@ -34,9 +39,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function addReply($reply)
