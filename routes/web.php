3f154ef0bb75276e306c900b3df9f85e24bb2c56
LeeDark <sergey.povarnin@gmail.com>
2017-12-22 17:53:01 +0200

    23. A User can delete their Threads.

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //Route::resource('threads', 'ThreadsController');
 Route::get('/threads', 'ThreadsController@index');
-Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}', 'ThreadsController@index');
-Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
+Route::get('/threads/create', 'ThreadsController@create');
 Route::post('/threads', 'ThreadsController@store');
+Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
+Route::delete('/threads/{channel}/{thread}', 'ThreadsController@destroy');
 
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 
