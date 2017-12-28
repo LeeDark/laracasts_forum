0b17ceabfec552d3f7a70d60f8365aab83eb27d5
LeeDark <sergey.povarnin@gmail.com>
2017-12-28 21:06:02 +0200

    34. A Vue Favorite Component.

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -13,14 +13,27 @@ trait Favoritable
     public function favorite()
     {
         $attributes = ['user_id' => auth()->id()];
+
         if (!$this->favorites()->where($attributes)->exists()) {
             $this->favorites()->create($attributes);
         }
     }
 
+    public function unfavorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        $this->favorites()->where($attributes)->delete();
+    }
+
     public function isFavorited()
     {
         return !!$this->favorites->where('user_id', auth()->id())->count();
+    }
+
+    public function getIsFavoritedAttribute()
+    {
+        return $this->isFavorited();
     }
 
     public function getFavoritesCountAttribute()
